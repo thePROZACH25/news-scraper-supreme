58b071b027083b0612a4bd250841c46372795e37
Zach S.L <zach.s.ledford@gmail.com>
2020-06-19 05:54:27 -0400

    scraping data from website and adding to database.

--- controller/controller.js
+++ controller/controller.js
@@ -6,6 +6,7 @@ var cheerio = require("cheerio");
 
 var db = require("../models");
 
+// Loads home page
 router.get("/", function (req, res) {
   res.render("index");
 });
@@ -41,5 +42,33 @@ router.get("/scrape", function (req, res) {
     res.send("Scrape Complete");
   });
 });
+
+router.get("/articles", function (req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function (dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+router.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("comment")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
 module.exports = router;
